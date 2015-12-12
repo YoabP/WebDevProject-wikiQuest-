211dcbd74846e52bd90cf7f76315e93ca09b8436
Yoab Pizarro <yoab.pizarro@gmail.com>
2015-12-12 03:45:37 -0600

    parser using regex

--- client/app/wikiParser/wikiParser.filter.js
+++ client/app/wikiParser/wikiParser.filter.js
@@ -1,44 +1,34 @@
 'use strict';
-
+function unecesaryLink(match, p1,p2, offset, string) {
+  return (p1?('class="'+p1+' new"'):(' class="new"'))+' href';
+}
 angular.module('wikiQuestApp')
   .filter('wikiParser', function() {
     return function(input, title) {
       input = input || '';
-      var out = "";
-      var tempContainer = document.createElement('div');
-      tempContainer.innerHTML = input;
-      //patch a tags
-      var links = tempContainer.getElementsByTagName('a');
-      for (var i = 0; i < links.length; i++) {
-        var element = links[i];
-        var oldRef = element.getAttribute('href');
-        if(oldRef) {
-  				if (oldRef[0]=='#' && oldRef!='#'){
-            //preserve anchor
-            element.setAttribute('ng-click', 'anchor("'+ oldRef.substring(1) + '")');
-          }
-  				else if (oldRef[0]=='/' && oldRef[1]=='w' && oldRef[2]!='/') { //is internal
-  					var index = oldRef.lastIndexOf('/');
-  					var pageName= decodeURIComponent(oldRef.substring(index+1));
-            element.setAttribute('ng-click', 'innerLink("'+pageName.replace(/"/g, '\\"')+'")');
-  				}
-          else if (oldRef[0]=='/' && oldRef[1]=='w' && oldRef[2]=='/'){
-            var pageName = element.textContent;
-            element.setAttribute('ng-click', 'innerLink("'+pageName.replace(/"/g, '\\"')+'")');
-          }
-  				else {
-            element.classList.add('new');
-          }
-          element.setAttribute('href', '');
-  			}
-      }
-      //patch img tags
-      //pending ...
-      out = tempContainer.innerHTML;
+      var out = input;
+      //regex parsing Wikipedia:VECTOR
+      //filter media Files
+      out = out.replace(/href="\/wiki\/(?:File|Special|Template):.*?"/g, unecesaryLink);
+      //  filter inner links
+      out = out.replace(/href="\/wiki\/(.+?)(?:#.*?|)"/g, function (match, p1, offset, string) {
+        return 'ng-click="innerLink(\''+decodeURIComponent(p1)
+          .replace(/'/g,"\\'")
+          .replace(/"/g,'&quot;')
+          +'\')" href';
+      });
+      //Filter wiki links to uneeded tools
+      out = out.replace(/(?:class="(.*?)" )?href="\/w\/(.*?)"/g, unecesaryLink);
+      out = out.replace(/(?:class="(.*?)" )?href="\/\/(.*?)"/g, unecesaryLink);
+      //filter anchors
+      out = out.replace(/href="#(.*?)"/g,'href ng-click="anchor(\'$1\')"');
+      //filter external links
+      out = out.replace(/(?:class="(.*?)" )?href="http.*?"/g, unecesaryLink);
+      //filter angular directive confusion
+      out = out.replace(/({\s*{[^}{]*?}\s*})/g,'<span ng-non-bindable>$1</span>');
       //append missing title header
       out = '<h1 id="firstHeading" class="firstHeading" lang="en">' +
             title.replace(/_/g,' ') +'</h1>' + out;
-      //escape possible angular {{ stuff }}
-      return out.replace(/({\s*{[^}{]*?}\s*})/g,'<span ng-non-bindable>$1</span>');
+      return out;
     };
   });
